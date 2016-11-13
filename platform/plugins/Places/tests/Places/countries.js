002af7aeb9fc739d599447e31dab61185aa17c27
Valeriya <lebedevavaleriya@gmail.com>
2016-11-13 21:57:13 +0200

    Have created function for validation of JSON here (../Places/web/js/lib/countries/)

--- platform/plugins/Places/tests/Places/countries.js
+++ platform/plugins/Places/tests/Places/countries.js
@@ -26,49 +26,54 @@ function parseJsData(filename) {
 		;
 	return JSON.parse(json);
 }
+function parseJSON(countries) {
+	//var cbc = Places.countriesByCode;
+	var cbc = ["AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS",
+		"BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "KH", "CM", "CA", "CV", "KY", "CF", "TD", "CL", "CN",
+		"CX", "CC", "CO", "KM", "CG", "CD", "CK", "CR", "CI", "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "ET", "FK", "FO", "FJ", "FI",
+		"FR", "GF", "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "HN", "HK", "HU", "IS", "IN", "ID", "IR",
+		"IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK", "MG", "MW",
+		"MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU",
+		"NF", "MP", "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS",
+		"SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH",
+		"TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "US", "UM", "UY", "UZ", "VU", "VA", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"];
 
-//var cbc = Places.countriesByCode;
-var cbc = ["AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS",
-	"BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "KH", "CM", "CA", "CV", "KY", "CF", "TD", "CL", "CN",
-	"CX", "CC", "CO", "KM", "CG", "CD", "CK", "CR", "CI", "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "ET", "FK", "FO", "FJ", "FI",
-	"FR", "GF", "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "HN", "HK", "HU", "IS", "IN", "ID", "IR",
-	"IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK", "MG", "MW",
-	"MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU",
-	"NF", "MP", "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS",
-	"SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH",
-	"TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "US", "UM", "UY", "UZ", "VU", "VA", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"];
+	// test
+	var diffResult;
+	var new_array = [];
 
-//I use this file for test purposes
-var countries = parseJsData('/projects/qbix/Q/platform/plugins/Places/tests/Places/countries/en.js');
+	Array.prototype.diff = function (a) {
+		return this.filter(function (i) {
+			return a.indexOf(i) < 0;
+		});
+	};
 
-// test
-var diffResult;
-var new_array = [];
+	for (i = 0; i < countries.length; i++) {
 
-Array.prototype.diff = function(a) {
-	return this.filter(function(i){return a.indexOf(i) < 0;});
-};
-
-for (i = 0; i < countries.length; i++) {
-
-	if (countries[i][1]) {
-		var pci = countries[i];
-		new_array[i] = pci[1];
+		if (countries[i][1]) {
+			var pci = countries[i];
+			new_array[i] = pci[1];
 
 
-		if (i == countries.length - 1) {
-			diffResult = cbc.diff(new_array); // comparing to arrays
-			console.log('Differences: ', diffResult);
+			if (i == countries.length - 1) {
+				diffResult = cbc.diff(new_array); // comparing to arrays
+				console.log('Differences: ', diffResult);
+			}
 		}
 	}
 }
+
+
+//I use this file for test purposes
+//var countries = parseJsData('/projects/qbix/Q/platform/plugins/Places/tests/Places/countries/en.js');
+//parseJSON(countries);
+
 
 var express = require('express');
 var exec = require("child_process").exec;
 
 exec('php /projects/qbix/Q/platform/plugins/Places/tests/Places/countries.php', function(error, stdout, stderr) {
 	console.log('stdout: ', stdout);
-	console.log('stderr: ', stderr);
 	if (error !== null) {
 		console.log('exec error: ', error);
 	}
--- platform/plugins/Places/tests/Places/countries.php
+++ platform/plugins/Places/tests/Places/countries.php
@@ -1,53 +1,104 @@
 <?php
 
-function Places_check_validation($language)
+function Places_check_validation($countries, $cbc1)
 {
-    //$url = "plugins/Places/js/lib/countries.js";
-    $url = "/projects/qbix/Q/platform/plugins/Places/tests/Places/countries/en.js";
-     //var cbc = Places.countriesByCode;
+    //var cbc = Places.countriesByCode;
     $cbc = ["AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS",
-    "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "KH", "CM", "CA", "CV", "KY", "CF", "TD", "CL", "CN",
-    "CX", "CC", "CO", "KM", "CG", "CD", "CK", "CR", "CI", "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "ET", "FK", "FO", "FJ", "FI",
-    "FR", "GF", "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "HN", "HK", "HU", "IS", "IN", "ID", "IR",
-    "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK", "MG", "MW",
-    "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU",
-    "NF", "MP", "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS",
-    "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH",
-    "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "US", "UM", "UY", "UZ", "VU", "VA", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"];
+        "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "KH", "CM", "CA", "CV", "KY", "CF", "TD", "CL", "CN",
+        "CX", "CC", "CO", "KM", "CG", "CD", "CK", "CR", "CI", "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "ET", "FK", "FO", "FJ", "FI",
+        "FR", "GF", "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "HN", "HK", "HU", "IS", "IN", "ID", "IR",
+        "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK", "MG", "MW",
+        "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU",
+        "NF", "MP", "NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN", "LC", "MF", "PM", "VC", "WS",
+        "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH",
+        "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "US", "UM", "UY", "UZ", "VU", "VA", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"];
 
-    //$dir = '/projects/qbix/Q/platform/plugins/Places/web/js/lib/countries/';
-    $dir = '/projects/qbix/Q/platform/plugins/Places/tests/Places/countries/';
+    if ($countries) {
+        $json_result = json_decode($countries, true);
+        if (is_null($json_result)) {
+            echo "Something dun gone blowed up!\n JSON Error:";
+            switch (json_last_error()) {
+                case JSON_ERROR_NONE:
+                    echo ' - No error has occurred';
+                    break;
+                case JSON_ERROR_DEPTH:
+                    echo ' - The maximum stack depth has been exceeded';
+                    break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    echo ' - Invalid or malformed JSON';
+                    break;
+                case JSON_ERROR_CTRL_CHAR:
+                    echo ' - Control character error, possibly incorrectly encoded';
+                    break;
+                case JSON_ERROR_SYNTAX:
+                    echo ' - Syntax error';
+                    break;
+                case JSON_ERROR_UTF8:
+                    echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                    break;
+                default:
+                    echo ' - Unknown error';
+                    break;
+            }
 
-    $countruFiles = array_diff(scandir($dir), array('..', '.'));
-    print_r($countruFiles);
+            echo PHP_EOL;
+        } else {
+            $error = [];
+            foreach ($json_result as $key => $val) {
+                if ($cbc[$key] == $val[1])
+                    echo '';  //echo "cbc= $cbc[$key]  val = $val[1]\n\n";
+                else {
+                    $error[] = "For '$val[0]' code = '$val[1]' but standard = '$cbc[$key]' \n\n";
 
-    $results = array();
-    if ($language) {
-        foreach ($countruFiles as $key => $val) {
-            if ($val) {
-                $urlFile = $dir . $val;
-                var_dump($urlFile);
-                try {
-                    $convert = file_get_contents($urlFile); //read the file
-
-                     if ( count($convert)) {
-                         $result = json_decode($urlFile);
-                         var_dump($result);
-
-                        // var_dump(array_udiff($cbc, $convert));
-                     }else{
-                         print_r(' else!!! ');
-                     }
-                } catch (Exception $e) {
-                    // error message
                 }
+            }
+            if (!empty($error)) {
+                print_r($error);
+            } else {
+                echo "No errors are detected\n";
             }
         }
 
     }
 }
 
-Places_check_validation('ua');
+function Places_find_file()
+{
+    //$dir = '/projects/qbix/Q/platform/plugins/Places/tests/Places/countries/';
+    $dir = '/projects/qbix/Q/platform/plugins/Places/web/js/lib/countries/';
+    $countryFiles = array_diff(scandir($dir), array('..', '.')); //get all files from countries folder
+
+    foreach ($countryFiles as $key => $val) {
+        preg_match("/^.*\.js$/", $val, $output_array); // use only .js file
+
+        if ($output_array) {
+            $urlFile = $dir . $val;
+            try {
+                $string = file_get_contents($urlFile);
+                //gets counries and counrie's code
+                preg_match_all("/(\[(\s+\[.*\],?)+\s+\])|({(\s+\\\".*\\\",?)+\s+})/", $string, $matches);
+
+                if (is_null($matches)) {
+                    echo "\nFile : $val\n";
+                    echo "Something dun gone blowed up!";
+                } else {
+                    echo "\n********** File : $val **********\n";
+
+                    //gets and sorts counrie's code
+                    $cCode = json_decode($matches[0][1], true);
+                    $cCode = array_keys($cCode);
+                    array_multisort($cCode, SORT_ASC);
+
+                    Places_check_validation($matches[0][0], $cCode);
+                }
+            } catch (Exception $e) {
+                echo "\n error message = $e\n";
+            }
+        }
+    }
+}
+
+Places_find_file();
 
 
 /*
@@ -64,7 +115,7 @@ class Places_Check
         $url = "plugins/Places/js/lib/countries.js";
         $results = array();
         if ($language) {
-            $url = "plugins/Places/js/lib/countries/$language.js"; //"https://pixabay.com/api/?key=$key&q=$keywords&$optionString";
+            $url = "plugins/Places/js/lib/countries/$language.js";
             $json = @file_get_contents($url);
             //$data = Q::json_decode($json, true);
 
