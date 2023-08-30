5b6b572678519e79e5e6e86a5e883980d132c840
Andrey Tepaykin <andrey@qbix.com>
2023-08-30 14:36:36 +0300

    Revert "Fixed bug when click on Pay button scroll down instead open dialog."
    This reverts commit cba26c14c4b1a852b754cced58bf62ba3840e519.

--- platform/plugins/Streams/web/js/tools/chat.js
+++ platform/plugins/Streams/web/js/tools/chat.js
@@ -923,6 +923,9 @@ Q.Tool.define('Streams/chat', function(options) {
 		// when virtual keyboard appear, trying to scroll body to input element position
 		$input.on('focus', function () {
 			tool.scrollToBottom();
+			setTimeout(function () {
+				tool.scrollToBottom();
+			}, 500);
 		});
 
 		// submit button handler
